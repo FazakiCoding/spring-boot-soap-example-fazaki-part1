a50e2351addedfcb832aa4f2715d90a53035fea7
fati <kiboufati@outlook.fr>
2020-04-08 21:13:28 +0100

    first commit

---
+++ src/main/java/com/fazaki/springbootsoapexamplefazaki/SpringBootSoapExampleFazakiApplication.java
@@ -0,0 +1,13 @@
+package com.fazaki.springbootsoapexamplefazaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapExampleFazakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapExampleFazakiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fazaki/springbootsoapexamplefazaki/SpringBootSoapExampleFazakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazaki.springbootsoapexamplefazaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapExampleFazakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
